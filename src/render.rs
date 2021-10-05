bc6a780d2ede3b211ef553d7663c263f67f56b48
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2021-10-05 18:30:18 +0200

    #49694, Add webgl2 support via the webgl2 feature

--- src/render.rs
+++ src/render.rs
@@ -24,8 +24,10 @@ pub fn setup(
 pub const SVG_PIPELINE_HANDLE: HandleUntyped =
     HandleUntyped::weak_from_u64(PipelineDescriptor::TYPE_UUID, 8514826620251853414);
 
+#[cfg(not(feature = "webgl2"))]
 const VERTEX_SHADER: &str = r#"
 #version 450
+
 layout(location = 0) in vec3 Vertex_Position;
 layout(location = 1) in vec4 Vertex_Color;
 
@@ -44,10 +46,50 @@ void main() {
 }
 "#;
 
+#[cfg(feature = "webgl2")]
+const VERTEX_SHADER: &str = r#"
+#version 300 es
+
+precision highp float;
+
+layout(location = 0) in vec3 Vertex_Position;
+layout(location = 1) in vec4 Vertex_Color;
+
+out vec4 v_color;
+
+layout(std140) uniform CameraViewProj {
+    mat4 ViewProj;
+};
+layout(std140) uniform Transform {
+    mat4 Model;
+};
+
+void main() {
+    gl_Position = ViewProj * Model * vec4(Vertex_Position, 1.0);
+    v_color = Vertex_Color;
+}
+"#;
+
+#[cfg(not(feature = "webgl2"))]
 const FRAGMENT_SHADER: &str = r#"
 #version 450
+
 layout(location = 0) in vec4 v_color;
 layout(location = 0) out vec4 o_Target;
+
+void main() {
+    o_Target = v_color;
+}
+"#;
+
+#[cfg(feature = "webgl2")]
+const FRAGMENT_SHADER: &str = r#"
+#version 300 es
+
+precision highp float;
+
+in vec4 v_color;
+out vec4 o_Target;
 
 void main() {
     o_Target = v_color;
